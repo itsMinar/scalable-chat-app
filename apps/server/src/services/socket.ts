e5c894265544a68437400cabd5fdc23cb0b6123e
Md. Minar Munshi <minarmunshi46@gmail.com>
2024-09-08 02:51:33 +0600

    Initialize the Server

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,30 @@
+import { Server } from 'socket.io';
+
+class SocketService {
+  private _io: Server;
+
+  constructor() {
+    console.log('Init Socket Server...');
+
+    this._io = new Server();
+  }
+
+  public initListener() {
+    const io = this.io;
+    console.log('Init Socket Listeners...');
+
+    io.on('connection', (socket) => {
+      console.log('new socket connected', socket.id);
+
+      socket.on('event:message', async ({ message }: { message: string }) => {
+        console.log('New message received', message);
+      });
+    });
+  }
+
+  get io() {
+    return this._io;
+  }
+}
+
+export default SocketService;
